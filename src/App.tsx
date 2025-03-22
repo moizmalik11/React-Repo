37669f5bb7c4bc838a15545ae6745165cc06d9a4
moizmalik11 <moiz.malik1113@gmail.com>
2025-03-22 17:22:47 +0500

    changed 2nd time

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,21 @@
-import { useState } from "react";
+import React, { Component } from 'react' 
 
-function SimpleForm() {
-  const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-  });
+export class App extends Component {
+  constructor(props: {}) {
+    super(props)
+  
+    this.state = {
+       
+    }
+  }
 
-  const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    alert("Form Submitted:");
-    // console.log("Form Submitted:", formData);
-  };
-
-  return (
-    <form onSubmit={handleSubmit}>
-      <label>Name:</label>
-      <input
-        type="text"
-        name="name"
-        value={formData.name}
-        onChange={handleChange}
-      />
-
-      <label>Email:</label>
-      <input
-        type="email"
-        name="email"
-        value={formData.email}
-        onChange={handleChange}
-      />
-
-      <button type="submit" onChange={handleSubmit}>Submit</button>
-
-      SimpleForm();
-    </form>
-  );
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
 }
 
-export default SimpleForm;
+export default App
